628ea61f75e16b110122eb7356e83453a7b76fda
Michal Lekstan <mlekstan@student.agh.edu.pl>
2024-04-02 15:18:14 +0200

    czesc ekranu logowania

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,12 +31,12 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.android.material:material:1.11.0")
 
 }
